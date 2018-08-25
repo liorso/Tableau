749856653cd1e9e6ae09aa419e978be516803ab2
danie <danielkatzan@gmail.com>
2018-08-25 21:26:32 +0300

    make tests more readable

--- main.py
+++ main.py
@@ -4,20 +4,35 @@ from formula import Formula
 
 
 def main():
-    formulas_strings = ['!(!(a))', '(a)A(b)', '!((a)O(b))', '!(X(a))', 'G(a)', '!((a)A(b))', '(a)O(b)', '(a)U(b)',
-                        '!((a)U(b))', '(!(a))A(!(X((a)U(b))))', '!(X(G(a)))', '(a)O(!(a))', '!((a)O(!(a)))',
-                        '(a)A(!(a))', '!((a)A(!(a)))', '((a)O(b))O(c)', '((a)A(!(a)))O((b)A(!(b)))',
-                        '((a)A(!(a)))O((a)A(!(a)))']
-    expected_results = [True, True, True, True, True, True, True, True, True, True, True, True, False, False, True,
-                        True, False, False]
+    test_cases = [('!(!(a))', True),
+                  ('(a)A(b)', True),
+                  ('!((a)O(b))', True),
+                  ('!(X(a))', True),
+                  ('G(a)', True),
+                  ('!((a)A(b))', True),
+                  ('(a)O(b)', True),
+                  ('(a)U(b)', True),
+                  ('!((a)U(b))', True),
+                  ('(!(a))A(!(X((a)U(b))))', True),
+                  ('!(X(G(a)))', True),
+                  ('(a)O(!(a))', True),
+                  ('!((a)O(!(a)))', False),
+                  ('(a)A(!(a))', False),
+                  ('!((a)A(!(a)))', True),
+                  ('((a)O(b))O(c)', True),
+                  ('((a)A(!(a)))O((b)A(!(b)))', False),
+                  ('((a)A(!(a)))O((a)A(!(a)))', False),
+                  ('(((a)A(!(a)))O((a)A(!(a))))A(c)', False),
+                  ('(((a)A(!(a)))O((a)A(!(a))))O(c)', True)
+                  ]
 
     errors = []
 
-    assert len(expected_results) == len(formulas_strings)
     iterations = 10
 
     for i in range(iterations):
-        for formula_string, expected_result in zip(formulas_strings, expected_results):
+        for test_case in test_cases:
+            formula_string, expected_result = test_case
 
             dfs_result = DfsTableau(Formula(formula_string), debug=False).build_tableau()
             bfs_result = BfsTableau(Formula(formula_string), debug=False).build_tableau()
